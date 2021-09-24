36940e1b9e7191e1aa50660b42862756edb58707
karvepushkaraj <karvepushkaraj@gmail.com>
2021-09-24 15:59:29 +0530

    4th commit
    changed the service & controller design.

--- LMS/src/main/java/com/app/lms/controller/LibraryManagementController.java
+++ LMS/src/main/java/com/app/lms/controller/LibraryManagementController.java
@@ -6,53 +6,66 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.app.lms.model.LibrarySection;
-import com.app.lms.service.LibrarySectionService;
+import com.app.lms.model.BookCopy;
+import com.app.lms.model.BookTitle;
+import com.app.lms.service.BookService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @RestController
-@RequestMapping("api/v1/lms/librarysection")
-public class LibrarySectionController {
-	
+@RequestMapping("api/v1/lms/books")
+public class BookController {
+
 	@Autowired
-	@Qualifier("LibrarySectionService")
-	private LibrarySectionService librarySectionService;
-	
-	@GetMapping
-	public LibrarySection getLibrarySection(@RequestParam("id") String id) {
-		return librarySectionService.getLibrarySection(id);
-	}
-	
-//	@GetMapping
-//	public String getBookSection(@RequestParam("id") String id) throws JsonProcessingException {
-//		BookSection bd = bookSectionService.getBookSection(id);
-//		return new ObjectMapper().writeValueAsString(bd);
-//	}
+	@Qualifier("BookService")
+	private BookService bookService;
 	
 	@PostMapping
-	public void addLibrarySection(@RequestBody LibrarySection bookSection) {
-		librarySectionService.addLibrarySection(bookSection);
+	public void addBook(@RequestBody String input) throws JsonMappingException, JsonProcessingException {
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode jsonNode = mapper.readTree(input);
+		String sectionId = jsonNode.get("sectionId").asText();
+		BookTitle bt = mapper.treeToValue(jsonNode.get("BookTitle"), BookTitle.class);
+		BookCopy bc = mapper.treeToValue(jsonNode.get("BookCopy"), BookCopy.class);
+		bookService.addBook(sectionId, bt, bc);
 	}
 	
-//	@PostMapping
-//	public void addBookSection(@RequestBody String bookSection) throws JsonMappingException, JsonProcessingException {
-//		ObjectMapper obj = new ObjectMapper();
-//		BookSection bs = obj.readValue(bookSection, BookSection.class);
-//		bookSectionService.addBookSection(bs);
-//	}
-	
-	@PutMapping
-	public void updateLibrarySection(@RequestBody LibrarySection bookSection) {
-		librarySectionService.updateLibrarySection(bookSection);
+	@GetMapping
+	public String getBook(@RequestParam("id") String bookId) throws JsonProcessingException {
+		ObjectMapper mapper = new ObjectMapper();
+		BookTitle bt = null;
+		BookCopy bc = null;
+		if(bookId.length()==4) {
+			bt = bookService.getBookTitle(bookId);
+			ObjectNode objectNode = mapper.valueToTree(bt);
+			ArrayNode arrayNode = objectNode.putArray("bookCopies");
+			for(BookCopy bookCopy : bt.getBookCopies()) {
+				ObjectNode node = mapper.createObjectNode();
+				node.put("copyId", bookCopy.getCopyId());
+				node.put("price", bookCopy.getPrice());
+				arrayNode.add(node);
+			}
+			return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(objectNode);
+		}
+		else if(bookId.length()==5) {
+			bc = bookService.getBookCopy(bookId);
+			return mapper.writeValueAsString(bc);
+		}
+		return null;
 	}
-
+	
 	@DeleteMapping("/{id}")
-	public void deleteLibrarySection(@PathVariable("id") String id) {
-		librarySectionService.deleteLibrarySection(id);
+	public void deleteBook(@PathVariable("id") String bookId) {
+		bookService.deleteBook(bookId);
 	}
+	
 }
